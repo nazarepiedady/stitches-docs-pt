703555ecb85e6ef13f47114096338dce5cb2cab9
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-10 15:19:23 +0100

    docs(pt): translate `lib/docsRoutes.ts`

--- lib/docsRoutes.ts
+++ lib/docsRoutes.ts
@@ -1,37 +1,37 @@
 export const docsRoutes = [
   {
-    label: 'Overview',
+    label: 'Visão Geral',
     pages: [
-      { title: 'Introduction', slug: 'docs/introduction' },
-      { title: 'Tutorials', slug: 'docs/tutorials' },
+      { title: 'Introdução', slug: 'docs/introduction' },
+      { title: 'Lições Práticas', slug: 'docs/tutorials' },
       { title: 'API', slug: 'docs/api' },
-      { title: 'Frequently asked questions', slug: 'docs/frequently-asked-questions' },
-      { title: 'Benchmarks', slug: 'docs/benchmarks' },
+      { title: 'Questões feitas com frequência', slug: 'docs/frequently-asked-questions' },
+      { title: 'Pontos de Referência', slug: 'docs/benchmarks' },
       { title: 'TypeScript', slug: 'docs/typescript' },
     ],
   },
 
   {
-    label: 'Getting Started',
+    label: 'Começar',
     pages: [
-      { title: 'Installation', slug: 'docs/installation' },
-      { title: 'Styling', slug: 'docs/styling' },
-      { title: 'Variants', slug: 'docs/variants' },
-      { title: 'Responsive styles', slug: 'docs/responsive-styles' },
-      { title: 'Overriding styles', slug: 'docs/overriding-styles' },
-      { title: 'Composing components', slug: 'docs/composing-components' },
-      { title: 'Framework agnostic API', slug: 'docs/framework-agnostic' },
+      { title: 'Instalação', slug: 'docs/installation' },
+      { title: 'Estilização', slug: 'docs/styling' },
+      { title: 'Variantes', slug: 'docs/variants' },
+      { title: 'Estilos moldáveis', slug: 'docs/responsive-styles' },
+      { title: 'Sobreposição de estilos', slug: 'docs/overriding-styles' },
+      { title: 'Composição de Componentes', slug: 'docs/composing-components' },
+      { title: 'API de abstração agnóstica', slug: 'docs/framework-agnostic' },
     ],
   },
 
   {
-    label: 'Configuration',
+    label: 'Configuração',
     pages: [
-      { title: 'Theme tokens', slug: 'docs/tokens' },
-      { title: 'Custom theming', slug: 'docs/theming' },
-      { title: 'Breakpoints', slug: 'docs/breakpoints' },
-      { title: 'Utils', slug: 'docs/utils' },
-      { title: 'Server-side rendering', slug: 'docs/server-side-rendering' },
+      { title: 'Simbólicos de tema', slug: 'docs/tokens' },
+      { title: 'Criação de temas personalizados', slug: 'docs/theming' },
+      { title: 'Pontos de quebra', slug: 'docs/breakpoints' },
+      { title: 'Utilitários', slug: 'docs/utils' },
+      { title: 'Interpretação no lado do servidor', slug: 'docs/server-side-rendering' },
     ],
   },
 ];
